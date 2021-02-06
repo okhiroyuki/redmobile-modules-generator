bfb704950872cbfcd41e902f4815e706bced1c30
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-02-06 10:05:52 +0900

    update package.json

--- prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'start prepare.sh'
-rm -rf dist && mkdir dist
-rm -rf node_modules
-rm package-lock.json
-npm i --production
-./node_modules/.bin/modclean -r --ignore="example*,examples"
-echo 'finish prepare.sh'
